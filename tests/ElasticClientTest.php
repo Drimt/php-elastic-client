a23b67eaf0d4642d8f13a139a4e2158ea23ab042
Thibault Debatty <thibault.debatty@proton.me>
2026-02-01 14:58:57 +0100

    fix phpcs

--- tests/ElasticClientTest.php
+++ tests/ElasticClientTest.php
@@ -18,15 +18,16 @@ class ElasticClientTest extends TestCase
         parent::setUpBeforeClass();
         $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
         $dotenv->load();
-    } 
+    }
     
     
     public function testSetUserPassword()
     {
         $client = new ElasticClient("http://127.0.0.1:9200", "elastic", $_ENV["ES_PASSWORD"]);
         $result = $client->post(
-                "/_security/user/kibana_system/_password",
-                ['password' => $_ENV["KIBANA_PASSWORD"]]);
+            "/_security/user/kibana_system/_password",
+            ['password' => $_ENV["KIBANA_PASSWORD"]]
+        );
         
         $this->assertEquals([], $result);
     }
